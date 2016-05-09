a51572fab535e9fb861182c0dbffcb59fa0fbab5
songsiqi <songsiqi2006@126.com>
2016-05-09 23:08:28 +0800

    upgrade px2rem version to support keyframes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # px2rem-browser
 
-v0.1.3
+v0.1.5
 
 This is a web version of [px2rem](https://www.npmjs.com/package/px2rem) which could run in a browser.
 
@@ -9,16 +9,15 @@ This is a web version of [px2rem](https://www.npmjs.com/package/px2rem) which co
 ```
 <script src="./build/px2rem.js"></script>
 <script>
-    var Px2rem = require('px2rem');
-    var px2rem = new Px2rem();
-    var originCssText = '...';
-    var dpr = 2;
-    var newCssText = px2remIns.generateRem(originCssText); // generate rem version stylesheet
-    var newCssText = px2remIns.generateThree(originCssText, dpr); // generate @1x, @2x and @3x version stylesheet
+  var Px2rem = require('px2rem');
+  var px2rem = new Px2rem();
+  var originCssText = '...';
+  var dpr = 2;
+  var newCssText = px2remIns.generateRem(originCssText); // generate rem version stylesheet
+  var newCssText = px2remIns.generateThree(originCssText, dpr); // generate @1x, @2x and @3x version stylesheet
 </script>
 ```
 
 ## Build
 
 Run `npm start` command.
-
--- build/px2rem.debug.js
+++ build/px2rem.debug.js
@@ -238,6 +238,9 @@ var currentQueue;
 var queueIndex = -1;
 
 function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
     draining = false;
     if (currentQueue.length) {
         queue = currentQueue.concat(queue);
@@ -328,156 +331,171 @@ var css = require('css');
 var extend = require('extend');
 
 var defaultConfig = {
-    baseDpr: 2,             // base device pixel ratio (default: 2)
-    remUnit: 75,            // rem unit value (default: 75)
-    remPrecision: 6,        // rem value precision (default: 6)
-    forcePxComment: 'px',   // force px comment (default: `px`)
-    keepComment: 'no'       // no transform value comment (default: `no`)
+  baseDpr: 2,             // base device pixel ratio (default: 2)
+  remUnit: 75,            // rem unit value (default: 75)
+  remPrecision: 6,        // rem value precision (default: 6)
+  forcePxComment: 'px',   // force px comment (default: `px`)
+  keepComment: 'no'       // no transform value comment (default: `no`)
 };
 
 var pxRegExp = /\b(\d+(\.\d+)?)px\b/;
 
 function Px2rem(options) {
-    this.config = {};
-    extend(this.config, defaultConfig, options);
+  this.config = {};
+  extend(this.config, defaultConfig, options);
 }
 
 // generate @1x, @2x and @3x version stylesheet
-Px2rem.prototype.generateThree = function(cssText, dpr) {
-    dpr = dpr || 2;
-    var self = this;
-    var config = self.config;
-    var astObj = css.parse(cssText);
+Px2rem.prototype.generateThree = function (cssText, dpr) {
+  dpr = dpr || 2;
+  var self = this;
+  var config = self.config;
+  var astObj = css.parse(cssText);
 
-    function processRules(rules) {
-        for (var i = 0; i < rules.length; i++) {
-            var rule = rules[i];
-            if (rule.type === 'media') {
-                processRules(rule.rules); // recursive invocation while dealing with media queries
-                continue;
-            } else if (rule.type !== 'rule') {
-                continue;
-            }
+  function processRules(rules) {
+    for (var i = 0; i < rules.length; i++) {
+      var rule = rules[i];
+      if (rule.type === 'media') {
+        processRules(rule.rules); // recursive invocation while dealing with media queries
+        continue;
+      } else if (rule.type === 'keyframes') {
+        processRules(rule.keyframes); // recursive invocation while dealing with keyframes
+        continue;
+      } else if (rule.type !== 'rule' && rule.type !== 'keyframe') {
+        continue;
+      }
 
-            var declarations = rule.declarations;
-            for (var j = 0; j < declarations.length; j++) {
-                var declaration = declarations[j];
-                // need transform: declaration && has 'px'
-                if (declaration.type === 'declaration' && pxRegExp.test(declaration.value)) {
-                    var nextDeclaration = rule.declarations[j + 1];
-                    if (nextDeclaration && nextDeclaration.type === 'comment') { // next next declaration is comment
-                        if (nextDeclaration.comment.trim() === config.keepComment) { // no transform
-                            declarations.splice(j + 1, 1); // delete corresponding comment
-                            continue;
-                        } else if (nextDeclaration.comment.trim() === config.forcePxComment) { // force px
-                            declarations.splice(j + 1, 1); // delete corresponding comment
-                        }
-                    }
-                    declaration.value = self._getCalcValue('px', declaration.value, dpr); // common transform
-                }
+      var declarations = rule.declarations;
+      for (var j = 0; j < declarations.length; j++) {
+        var declaration = declarations[j];
+        // need transform: declaration && has 'px'
+        if (declaration.type === 'declaration' && pxRegExp.test(declaration.value)) {
+          var nextDeclaration = rule.declarations[j + 1];
+          if (nextDeclaration && nextDeclaration.type === 'comment') { // next next declaration is comment
+            if (nextDeclaration.comment.trim() === config.keepComment) { // no transform
+              declarations.splice(j + 1, 1); // delete corresponding comment
+              continue;
+            } else if (nextDeclaration.comment.trim() === config.forcePxComment) { // force px
+              declarations.splice(j + 1, 1); // delete corresponding comment
             }
+          }
+          declaration.value = self._getCalcValue('px', declaration.value, dpr); // common transform
         }
+      }
     }
+  }
 
-    processRules(astObj.stylesheet.rules);
+  processRules(astObj.stylesheet.rules);
 
-    return css.stringify(astObj);
+  return css.stringify(astObj);
 };
 
 // generate rem version stylesheet
-Px2rem.prototype.generateRem = function(cssText) {
-    var self = this;
-    var config = self.config;
-    var astObj = css.parse(cssText);
+Px2rem.prototype.generateRem = function (cssText) {
+  var self = this;
+  var config = self.config;
+  var astObj = css.parse(cssText);
 
-    function processRules(rules) {
-        for (var i = 0; i < rules.length; i++) {
-            var rule = rules[i];
-            if (rule.type === 'media') {
-                processRules(rule.rules); // recursive invocation while dealing with media queries
-                continue;
-            } else if (rule.type !== 'rule') {
-                continue;
-            }
+  function processRules(rules, noDealPx) { // FIXME: keyframes do not support `force px` comment
+    for (var i = 0; i < rules.length; i++) {
+      var rule = rules[i];
+      if (rule.type === 'media') {
+        processRules(rule.rules); // recursive invocation while dealing with media queries
+        continue;
+      } else if (rule.type === 'keyframes') {
+        processRules(rule.keyframes, true); // recursive invocation while dealing with keyframes
+        continue;
+      } else if (rule.type !== 'rule' && rule.type !== 'keyframe') {
+        continue;
+      }
 
-            // generate 3 new rules which has [data-dpr]
-            var newRules = [];
-            for (var dpr = 1; dpr <= 3; dpr++) {
-                var newRule = {};
-                newRule.type = rule.type;
-                newRule.selectors = rule.selectors.map(function(sel) {
-                    return '[data-dpr="' + dpr + '"] ' + sel;
-                });
-                newRule.declarations = [];
-                newRules.push(newRule);
-            }
-
-            var declarations = rule.declarations;
-            for (var j = 0; j < declarations.length; j++) {
-                var declaration = declarations[j];
-                // need transform: declaration && has 'px'
-                if (declaration.type === 'declaration' && pxRegExp.test(declaration.value)) {
-                    var nextDeclaration = rule.declarations[j + 1];
-                    if (nextDeclaration && nextDeclaration.type === 'comment') { // next next declaration is comment
-                        if (nextDeclaration.comment.trim() === config.forcePxComment) { // force px
-                            // do not transform `0px`
-                            if (declaration.value === '0px') {
-                                declaration.value = '0';
-                                declarations.splice(j + 1, 1); // delete corresponding comment
-                                continue;
-                            }
-                            // generate 3 new declarations and put them in the new rules which has [data-dpr]
-                            for (var dpr = 1; dpr <= 3; dpr++) {
-                                var newDeclaration = {};
-                                extend(true, newDeclaration, declaration);
-                                newDeclaration.value = self._getCalcValue('px', newDeclaration.value, dpr);
-                                newRules[dpr - 1].declarations.push(newDeclaration);
-                            }
-                            declarations.splice(j, 2); // delete this rule and corresponding comment
-                            j--;
-                        } else if (nextDeclaration.comment.trim() === config.keepComment) { // no transform
-                            declarations.splice(j + 1, 1); // delete corresponding comment
-                        } else {
-                            declaration.value = self._getCalcValue('rem', declaration.value); // common transform
-                        }
-                    } else {
-                        declaration.value = self._getCalcValue('rem', declaration.value); // common transform
-                    }
-                }
-            }
-
-            // if the origin rule has no declarations, delete it
-            if (!rules[i].declarations.length) {
-                rules.splice(i, 1);
-                i--;
-            }
-
-            // add the new rules which contain declarations that are forced to use px
-            if (newRules[0].declarations.length) {
-                rules.splice(i + 1, 0, newRules[0], newRules[1], newRules[2]);
-                i += 3; // skip the added new rules
-            }
+      if (!noDealPx) {
+        // generate 3 new rules which has [data-dpr]
+        var newRules = [];
+        for (var dpr = 1; dpr <= 3; dpr++) {
+          var newRule = {};
+          newRule.type = rule.type;
+          newRule.selectors = rule.selectors.map(function (sel) {
+            return '[data-dpr="' + dpr + '"] ' + sel;
+          });
+          newRule.declarations = [];
+          newRules.push(newRule);
         }
+      }
+
+      var declarations = rule.declarations;
+      for (var j = 0; j < declarations.length; j++) {
+        var declaration = declarations[j];
+        // need transform: declaration && has 'px'
+        if (declaration.type === 'declaration' && pxRegExp.test(declaration.value)) {
+          var nextDeclaration = rule.declarations[j + 1];
+          if (nextDeclaration && nextDeclaration.type === 'comment') { // next next declaration is comment
+            if (nextDeclaration.comment.trim() === config.forcePxComment) { // force px
+              // do not transform `0px`
+              if (declaration.value === '0px') {
+                declaration.value = '0';
+                declarations.splice(j + 1, 1); // delete corresponding comment
+                continue;
+              }
+              if (!noDealPx) {
+                // generate 3 new declarations and put them in the new rules which has [data-dpr]
+                for (var dpr = 1; dpr <= 3; dpr++) {
+                  var newDeclaration = {};
+                  extend(true, newDeclaration, declaration);
+                  newDeclaration.value = self._getCalcValue('px', newDeclaration.value, dpr);
+                  newRules[dpr - 1].declarations.push(newDeclaration);
+                }
+                declarations.splice(j, 2); // delete this rule and corresponding comment
+                j--;
+              } else { // FIXME: keyframes do not support `force px` comment
+                declaration.value = self._getCalcValue('rem', declaration.value); // common transform
+                declarations.splice(j + 1, 1); // delete corresponding comment
+              }
+            } else if (nextDeclaration.comment.trim() === config.keepComment) { // no transform
+              declarations.splice(j + 1, 1); // delete corresponding comment
+            } else {
+              declaration.value = self._getCalcValue('rem', declaration.value); // common transform
+            }
+          } else {
+            declaration.value = self._getCalcValue('rem', declaration.value); // common transform
+          }
+        }
+      }
+
+      // if the origin rule has no declarations, delete it
+      if (!rules[i].declarations.length) {
+        rules.splice(i, 1);
+        i--;
+      }
+
+      if (!noDealPx) {
+        // add the new rules which contain declarations that are forced to use px
+        if (newRules[0].declarations.length) {
+          rules.splice(i + 1, 0, newRules[0], newRules[1], newRules[2]);
+          i += 3; // skip the added new rules
+        }
+      }
     }
+  }
 
-    processRules(astObj.stylesheet.rules);
+  processRules(astObj.stylesheet.rules);
 
-    return css.stringify(astObj);
+  return css.stringify(astObj);
 };
 
 // get calculated value of px or rem
-Px2rem.prototype._getCalcValue = function(type, value, dpr) {
-    var config = this.config;
-    var pxGlobalRegExp = new RegExp(pxRegExp.source, 'g');
+Px2rem.prototype._getCalcValue = function (type, value, dpr) {
+  var config = this.config;
+  var pxGlobalRegExp = new RegExp(pxRegExp.source, 'g');
 
-    function getValue(val) {
-        val = parseFloat(val.toFixed(config.remPrecision)); // control decimal precision of the calculated value
-        return val == 0 ? val : val + type;
-    }
+  function getValue(val) {
+    val = parseFloat(val.toFixed(config.remPrecision)); // control decimal precision of the calculated value
+    return val == 0 ? val : val + type;
+  }
 
-    return value.replace(pxGlobalRegExp, function($0, $1) {
-        return type === 'px' ? getValue($1 * dpr / config.baseDpr) : getValue($1 / config.remUnit);
-    });
+  return value.replace(pxGlobalRegExp, function ($0, $1) {
+    return type === 'px' ? getValue($1 * dpr / config.baseDpr) : getValue($1 / config.remUnit);
+  });
 };
 
 module.exports = Px2rem;
@@ -4642,9 +4660,10 @@ function urix(aPath) {
 module.exports = urix
 
 },{"path":2}],28:[function(require,module,exports){
+'use strict';
+
 var hasOwn = Object.prototype.hasOwnProperty;
 var toStr = Object.prototype.toString;
-var undefined;
 
 var isArray = function isArray(arr) {
 	if (typeof Array.isArray === 'function') {
@@ -4655,28 +4674,26 @@ var isArray = function isArray(arr) {
 };
 
 var isPlainObject = function isPlainObject(obj) {
-	'use strict';
 	if (!obj || toStr.call(obj) !== '[object Object]') {
 		return false;
 	}
 
-	var has_own_constructor = hasOwn.call(obj, 'constructor');
-	var has_is_property_of_method = obj.constructor && obj.constructor.prototype && hasOwn.call(obj.constructor.prototype, 'isPrototypeOf');
+	var hasOwnConstructor = hasOwn.call(obj, 'constructor');
+	var hasIsPrototypeOf = obj.constructor && obj.constructor.prototype && hasOwn.call(obj.constructor.prototype, 'isPrototypeOf');
 	// Not own constructor property must be Object
-	if (obj.constructor && !has_own_constructor && !has_is_property_of_method) {
+	if (obj.constructor && !hasOwnConstructor && !hasIsPrototypeOf) {
 		return false;
 	}
 
 	// Own properties are enumerated firstly, so to speed up,
 	// if last one is own, then all properties are own.
 	var key;
-	for (key in obj) {}
+	for (key in obj) {/**/}
 
-	return key === undefined || hasOwn.call(obj, key);
+	return typeof key === 'undefined' || hasOwn.call(obj, key);
 };
 
 module.exports = function extend() {
-	'use strict';
 	var options, name, src, copy, copyIsArray, clone,
 		target = arguments[0],
 		i = 1,
@@ -4703,25 +4720,23 @@ module.exports = function extend() {
 				copy = options[name];
 
 				// Prevent never-ending loop
-				if (target === copy) {
-					continue;
-				}
+				if (target !== copy) {
+					// Recurse if we're merging plain objects or arrays
+					if (deep && copy && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
+						if (copyIsArray) {
+							copyIsArray = false;
+							clone = src && isArray(src) ? src : [];
+						} else {
+							clone = src && isPlainObject(src) ? src : {};
+						}
 
-				// Recurse if we're merging plain objects or arrays
-				if (deep && copy && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
-					if (copyIsArray) {
-						copyIsArray = false;
-						clone = src && isArray(src) ? src : [];
-					} else {
-						clone = src && isPlainObject(src) ? src : {};
+						// Never move original objects, clone them
+						target[name] = extend(deep, clone, copy);
+
+					// Don't bring in undefined values
+					} else if (typeof copy !== 'undefined') {
+						target[name] = copy;
 					}
-
-					// Never move original objects, clone them
-					target[name] = extend(deep, clone, copy);
-
-				// Don't bring in undefined values
-				} else if (copy !== undefined) {
-					target[name] = copy;
 				}
 			}
 		}
